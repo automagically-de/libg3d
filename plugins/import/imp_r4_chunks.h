ef69ef2e6ce988ec8ec331e04d4d39efa5faefff
Markus Dahms <mad@automagically.de>
2006-12-05 13:26:31 +0000

    * some more chunk types (from Monzoom/Reflections 4.3)

--- plugins/import/imp_r4_chunks.h
+++ plugins/import/imp_r4_chunks.h
@@ -8,19 +8,24 @@
 static g3d_iff_chunk_info r4_chunks[] = {
 	{ "INFO", "information",                       0, NULL },
 	{ "DRE2", "triangles",                         0, r4_cb_DRE2 },
+	{ "DSP2", "unknown",                           0, NULL }, /* R4.3 */
+	{ "GLOW", "unknown",                           0, NULL }, /* R4.3 */
 	{ "GMA1", "material",                          0, r4_cb_GMAx },
 	{ "GMAT", "material",                          0, r4_cb_GMAx },
 	{ "KSYS", "coordinate system",                 0, r4_cb_KSYS },
 	{ "KUG1", "sphere",                            0, r4_cb_KUG1 },
 	{ "LGH3", "light",                             0, r4_cb_LGH3 },
 	{ "NEBL", "fog?",                              0, NULL },
+	{ "PKL1", "unknown",                           0, NULL }, /* R4.3 */
 	{ "PKTM", "points",                            0, r4_cb_PKTM },
 	{ "RGE1", "geometry",                          0, r4_cb_RGE1 },
 	{ "RKA2", "camera",                            0, r4_cb_RKAx },
 	{ "RKA3", "camera",                            0, r4_cb_RKAx },
 	{ "ROBJ", "object name",                       0, r4_cb_ROBJ },
+	{ "RSPG", "unknown",                           0, NULL }, /* R4.3 */
 	{ "SEQ1", "sequence",                          0, NULL },
 	{ "SEQ2", "sequence",                          0, NULL },
+	{ "SKE2", "unknown",                           0, NULL }, /* R4.3 */
 	{ "SUR1", "surface",                           0, r4_cb_SURx },
 	{ "SURF", "surface",                           0, r4_cb_SURx },
 	{ "TXM1", "unknown",                           0, r4_cb_TXM1 },

7ceec27cc03bf861bcf46e7e3686c01988abd579
Markus Dahms <mad@automagically.de>
2008-09-02 09:56:17 +0000

    * img_bmp: fixed - BGR instead of RGB

--- plugins/image/img_bmp.c
+++ plugins/image/img_bmp.c
@@ -33,18 +33,17 @@ gboolean plugin_load_image(G3DContext *context, const gchar *filename,
 	FILE *f;
 	guint32 filesize, offset, headsize, compression;
 	gint32 x, y, i;
+	guint32 ncolplanes, c;
 
 	f = fopen(filename, "r");
-	if(f == NULL)
-	{
+	if(f == NULL) {
 		g_printerr("couldn't open image file '%s': %s\n", filename,
 			strerror(errno));
 		return FALSE;
 	}
 
 	/* bitmap file always starts with 'BM' */
-	if(g3d_read_int16_le(f) != ('B' | ('M' << 8)))
-	{
+	if(g3d_read_int16_le(f) != ('B' | ('M' << 8))) {
 		g_printerr("bitmap magic not found: image seems to be corrupt\n");
 		return FALSE;
 	}
@@ -57,50 +56,47 @@ gboolean plugin_load_image(G3DContext *context, const gchar *filename,
 	headsize = g3d_read_int32_le(f);      /* size of header */
 	image->width  = g3d_read_int32_le(f); /* width */
 	image->height = g3d_read_int32_le(f); /* height */
-	g3d_read_int16_le(f);                 /* num of color planes */
+	ncolplanes = g3d_read_int16_le(f);    /* num of color planes */
 	image->depth  = g3d_read_int16_le(f); /* bits per pixel */
 	compression   = g3d_read_int32_le(f); /* compression */
 	g3d_read_int32_le(f);                 /* image size */
 	g3d_read_int32_le(f);                 /* v/res (dpi) */
 	g3d_read_int32_le(f);                 /* h/res (dpi) */
 
+#if DEBUG > 0
+	g_debug("BMP: %dx%dx%d (%d, 0x%x)", image->width, image->height,
+		image->depth, ncolplanes, compression);
+#endif
+
 	fseek(f, offset, SEEK_SET);
 
 #define ALL32BIT
 #ifndef ALL32BIT /* always 32bit for now.. */
-	image->pixeldata = g_malloc0(image->width*image->height*(image->depth/8));
+	image->pixeldata = g_new0(guint8,
+		image->width * image->height * (image->depth / 8));
 #else
-	image->pixeldata = g_malloc0(image->width * image->height * 4);
+	image->pixeldata = g_new0(guint8, image->width * image->height * 4);
 #endif
 
-	for(y=image->height-1; y>=0; y--)
-	{
-#if DEBUG > 5
-		g_printerr("| ");
-#endif
-		for(x=0; x<image->width; x++)
-		{
-			switch(image->depth)
-			{
+	for(y = (image->height - 1); y >= 0; y --) {
+		for(x = 0; x < image->width; x ++) {
+			switch(image->depth) {
 				case 8:
 #ifndef ALL32BIT
 					image->pixeldata[y*image->width+x] = g3d_read_int8(f);
 #else
-					image->pixeldata[(y*image->width+x)*4+0] = g3d_read_int8(f);
-					image->pixeldata[(y*image->width+x)*4+1] =
-					image->pixeldata[(y*image->width+x)*4+2] =
-						image->pixeldata[(y*image->width+x)*4+0];
-					image->pixeldata[(y*image->width+x)*4+3] = 0xFF;
-#endif
-#if DEBUG > 5
-					g_printerr("%2.2X ", image->pixeldata[y*image->width+x]);
+					c = g3d_read_int8(f);
+					for(i = 0; i < 3; i ++)
+						image->pixeldata[(y * image->width + x) * 4 + i] = c;
+					image->pixeldata[(y * image->width + x) * 4 + 3] = 0xFF;
 #endif
 					break;
 				case 24:
-					image->pixeldata[(y*image->width+x)*4+0] = g3d_read_int8(f);
-					image->pixeldata[(y*image->width+x)*4+1] = g3d_read_int8(f);
-					image->pixeldata[(y*image->width+x)*4+2] = g3d_read_int8(f);
-					image->pixeldata[(y*image->width+x)*4+3] = 0xFF;
+					/* read BGR */
+					for(i = 2; i >= 0; i --)
+						image->pixeldata[(y * image->width + x) * 4 + i] =
+							g3d_read_int8(f);
+					image->pixeldata[(y * image->width + x) * 4 + 3] = 0xFF;
 					break;
 				default:
 					break;

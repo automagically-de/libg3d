b3704470c08838b3d1c00f9486e1570e2f10b619
Markus Dahms <mad@automagically.de>
2008-09-09 12:20:17 +0000

    * imp_nff: converted to G3DStream

--- plugins/import/imp_nff/imp_nff.c
+++ plugins/import/imp_nff/imp_nff.c
@@ -26,6 +26,7 @@
 #include <locale.h>
 
 #include <g3d/g3d.h>
+#include <g3d/stream.h>
 
 /*****************************************************************************/
 /* plugin interface                                                          */
@@ -41,50 +42,39 @@
 #define NFF_SEC_POLYGON		  7
 #define NFF_SEC_POLPATCH	  8
 
-static gboolean nff_readline(FILE *f, char *line, int maxlen);
+static gboolean nff_readline(G3DStream *stream, gchar *line, guint32 maxlen);
 
-gboolean plugin_load_model(G3DContext *context, const gchar *filename,
+gboolean plugin_load_model_from_stream(G3DContext *context, G3DStream *stream,
 	G3DModel *model, gpointer plugin_data)
 {
-	FILE *f;
 	gchar line[1024];
 	G3DObject *object;
 	G3DMaterial *material = NULL;
+	G3DFace *face;
 	guint32 section = NFF_SEC_NOSECTION;
 	gfloat r,g,b, Kd, Ks, Sh, T, refr;
 	gfloat v1,v2,v3, n1,n2,n3;
-	gint32 i, num;
+	gint32 i, num, index;
+	gchar name[128];
 
 	setlocale(LC_NUMERIC, "C");
 
 	g_return_val_if_fail(model != NULL, FALSE);
 
-	f = fopen(filename, "r");
-	if(f == NULL)
-	{
-		g_warning("can't open file '%s'", filename);
-		return FALSE;
-	}
-
 	object = g_new0(G3DObject, 1);
 	object->name = g_strdup("NFF Object");
 	model->objects = g_slist_append(model->objects, object);
 
-	while(nff_readline(f, line, 1024))
-	{
+	while(nff_readline(stream, line, 1024)) {
 #if DEBUG > 3
 		g_printerr("DEBUG: %s\n", line);
 #endif
-		if(strcmp(line, "v") == 0) { section = NFF_SEC_VIEWPOINT; }
-		else if(sscanf(line, "b %g %g %g", &r, &g, &b) == 3)
-		{
+		if(strcmp(line, "v") == 0) {
+			section = NFF_SEC_VIEWPOINT;
+		} else if(sscanf(line, "b %g %g %g", &r, &g, &b) == 3) {
 			g3d_context_set_bgcolor(context, r, g, b, 1.0);
-		}
-		else if(sscanf(line, "f %g %g %g %g %g %g %g %g",
-			&r, &g, &b, &Kd, &Ks, &Sh, &T, &refr) == 8)
-		{
-			char name[128];
-
+		} else if(sscanf(line, "f %g %g %g %g %g %g %g %g",
+			&r, &g, &b, &Kd, &Ks, &Sh, &T, &refr) == 8) {
 			material = g3d_material_new();
 			material->r = r;
 			material->g = g;
@@ -99,11 +89,9 @@ gboolean plugin_load_model(G3DContext *context, const gchar *filename,
 			g_snprintf(name, 128, "material #%d",
 				g_slist_length(object->materials));
 			material->name = g_strdup(name);
-		}
-		else if((sscanf(line, "pp %d", &num) == 1) ||
-			(sscanf(line, "p %d", &num) == 1))
-		{
-			G3DFace *face = g_new0(G3DFace, 1);
+		} else if((sscanf(line, "pp %d", &num) == 1) ||
+			(sscanf(line, "p %d", &num) == 1)) {
+			face = g_new0(G3DFace, 1);
 
 			if(strncmp(line, "pp", 2) == 0) section = NFF_SEC_POLPATCH;
 			else section = NFF_SEC_POLYGON;
@@ -116,26 +104,21 @@ gboolean plugin_load_model(G3DContext *context, const gchar *filename,
 			face->vertex_count = num;
 			face->vertex_indices = g_malloc0(num * sizeof(guint32));
 			object->faces = g_slist_prepend(object->faces, face);
-			if(section == NFF_SEC_POLPATCH)
-			{
+			if(section == NFF_SEC_POLPATCH) {
 				face->flags |= G3D_FLAG_FAC_NORMALS;
 				face->normals = g_malloc0(num * 3 * sizeof(gfloat));
 			}
 			/* most faces are in this direction, but there are wrong models */
-			for(i=num-1; i>=0; i--)
-			{
-				if(!nff_readline(f, line, 1024))
-				{
-					fclose(f);
+			for(i = num - 1; i >= 0; i --) {
+				if(!nff_readline(stream, line, 1024)) {
 					g_printerr("reading vertices failed\n");
 					return FALSE;
 				}
 				if((section == NFF_SEC_POLPATCH) &&
 					 (sscanf(line, "%g %g %g %g %g %g",
 						&v1,&v2,&v3,
-						&n1,&n2,&n3) == 6))
-				{
-					int index = object->vertex_count - num + i;
+						&n1,&n2,&n3) == 6)) {
+					index = object->vertex_count - num + i;
 					object->vertex_data[index*3+0] = v1;
 					object->vertex_data[index*3+1] = v2;
 					object->vertex_data[index*3+2] = v3;
@@ -143,20 +126,17 @@ gboolean plugin_load_model(G3DContext *context, const gchar *filename,
 					face->normals[i*3+0] = -n1;
 					face->normals[i*3+1] = -n2;
 					face->normals[i*3+2] = -n3;
-				}
-				else if(sscanf(line, "%g %g %g", &v1,&v2,&v3) == 3)
-				{
-					int index = object->vertex_count - num + i;
+				} else if(sscanf(line, "%g %g %g", &v1,&v2,&v3) == 3) {
+					index = object->vertex_count - num + i;
 					object->vertex_data[index*3+0] = v1;
 					object->vertex_data[index*3+1] = v2;
 					object->vertex_data[index*3+2] = v3;
 					face->vertex_indices[i] = index;
-				}
-				else g_printerr("error in line '%s'\n", line);
+				} else
+					g_printerr("error in line '%s'\n", line);
 			}
 		}
 	}
-	fclose(f);
 	return TRUE;
 }
 
@@ -175,15 +155,15 @@ gchar **plugin_extensions(G3DContext *context)
 /* private functions                                                         */
 /*****************************************************************************/
 
-static gboolean nff_readline(FILE *f, char *line, int maxlen)
+static gboolean nff_readline(G3DStream *stream, gchar *line, guint32 maxlen)
 {
-	if(fgets(line, maxlen, f) == NULL) return FALSE;
+	if(g3d_stream_read_line(stream, line, maxlen) == NULL)
+		return FALSE;
 	g_strstrip(line);
 
-	if((strlen(line) == 0) || (line[0] == '#'))
-	{
+	if((strlen(line) == 0) || (line[0] == '#')) {
 		/* get next line if empty or comment */
-		return nff_readline(f, line, maxlen);
+		return nff_readline(stream, line, maxlen);
 	}
 	return TRUE;
 }

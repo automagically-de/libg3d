0b111c15050cafc80f4e7cdc4ff6b38524c27e91
Markus Dahms <mad@automagically.de>
2009-03-27 18:42:52 +0000

    * imp_lua: g3d.debug lua function

--- plugins/import/imp_lua/examples/test.lua
+++ plugins/import/imp_lua/examples/test.lua
@@ -1,3 +1,5 @@
 for i = 1, 10 do
 	g3d.hello('World')
 end
+
+g3d.debug('bla blubb')
--- plugins/import/imp_lua/imp_lua_funcs.c
+++ plugins/import/imp_lua/imp_lua_funcs.c
@@ -8,8 +8,17 @@ static int _g3d_hello(lua_State *ls)
 	return (0 != luaL_dostring(ls, "print 'Hello, World'"));
 }
 
+static int _g3d_debug(lua_State *ls)
+{
+	if(lua_isstring(ls, -1)) {
+		g_debug("lua: %s", lua_tolstring(ls, -1, NULL));
+	}
+	return 1;
+}
+
 static const luaL_Reg g3d_functions[] = {
 	{ "hello", _g3d_hello },
+	{ "debug", _g3d_debug },
 	{ NULL, NULL }
 };
 
